f6423f943a809e57efdbdc7fbd2104b5f965dd8e
Tailes14 <54458979+Tailes14@users.noreply.github.com>
2023-03-27 20:23:05 -0400

    pushing

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
 
 import MainMenu from "./MainMenu";
+import PoseDectector from "./temp(training)";
+import Training from "./Components/training";
+import detect from "./temp(App)";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(MainMenu);
+registerRootComponent(Training);
