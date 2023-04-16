a4ee7752fe34c708f268c244e8ec21f2d08fb69d
Tailes14 <54458979+Tailes14@users.noreply.github.com>
2023-04-16 14:23:46 -0400

    removed whitespace

--- helperFiles/CombineAllCSV.R
+++ helperFiles/CombineAllCSV.R
@@ -15,7 +15,3 @@ df = list.files(path = folder_path, full.names = TRUE, pattern = "\\.csv$") %>%
 # Export the combined data frame to a new CSV file
 output_file = file.path(folder_path, "All_Points.csv")
 write.csv(df, output_file, row.names = FALSE)
-
-
-
-
